a6b12db25a45b2ea6da49a18bb2623e718b09882
nguyenlyminhman <nguyenlyminhman@gmail.com>
2017-05-30 22:27:05 +0700

    stage 2

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.get('/', (req,res)=>res.render('home'));
+app.listen(3000, console.log('server is running...'));
