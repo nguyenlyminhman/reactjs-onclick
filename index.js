1be727af6bde9a57767fa57492024658e5e3fbbc
nguyenlyminhman <nguyenlyminhman@gmail.com>
2017-05-31 14:22:47 +0700

    stage 2

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const app = express();
+var express = require("express");
+var app = express();
+
 app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.set("views","./views");
+app.listen(3000, console.log("Server is running..."));
 
-app.set('view engine', 'ejs');
-app.set('views', './views');
-
-app.get('/', (req,res)=>res.render('home'));
-app.listen(3000, console.log('server is running...'));
+app.get("/", (req,res) => res.render("home"));
